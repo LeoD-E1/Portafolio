bc7d5329a15f45d277d04ec7ecf00e07b8dc7eeb
leoda <leodangeli11@gmail.com>
2021-07-19 12:48:50 -0300

    skill design changes, need to make a reusable component for buttons, stats and cards

--- src/components/SkillsList.js
+++ src/components/SkillsList.js
@@ -1,16 +1,13 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import { Heading, Image, Progress, Grid, GridItem, Flex, Text, Button } from '@chakra-ui/react'
-import { ArrowForwardIcon } from '@chakra-ui/icons'
-import { Link } from 'react-router-dom'
+import { Heading, Image, Progress, Grid, GridItem, Flex, Text } from '@chakra-ui/react'
 
-const LastSkills = () => {
+const SkillsList = () => {
 
-  const skillsCompleted = useSelector(state => state.skill)
-  const skills = skillsCompleted.filter(skill => skill.id >= skillsCompleted.length - 2)
+  const skills = useSelector(state => state.skill)
 
   return (
-    <Flex pt={5} px={3} justifyContent="center" h="50vh" alignItems="center"/* bgColor="#1B1F24" */>
+    <Flex pt={5} px={3} justifyContent="center" h="93vh" alignItems="center"/* bgColor="#1B1F24" */>
       <Flex direction="column" w={["90%", "80%", "70%", "60%"]}>
         <Heading mb={5} align="center">Some Skills</Heading>
         {
@@ -25,14 +22,9 @@ const LastSkills = () => {
             </Grid>
           ))
         }
-        <Link to="/skills" align="center">
-          <Button mt="10px" rightIcon={<ArrowForwardIcon />} variant="outline">
-            Skills
-          </Button>
-        </Link>
       </Flex>
     </Flex>
   )
 }
 
-export default LastSkills
+export default SkillsList
--- src/screens/Skills.js
+++ src/screens/Skills.js
@@ -1,26 +1,15 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import { Heading, Image, Progress, Grid, GridItem, Flex, Text } from '@chakra-ui/react'
+import React, { Fragment } from 'react'
+import SkillsList from '../components/SkillsList'
+import ButtonBack from '../components/ButtonBack'
+
 
 const Skills = () => {
 
-  const skills = useSelector(state => state.skill)
-
   return (
-    <Flex pt={5} px={3} justifyContent="center" h="93vh" alignItems="center"/* bgColor="#1B1F24" */>
-      <Flex direction="column" w={["90%", "80%", "70%", "60%"]}>
-        <Heading mb={5} align="center">Some Skills</Heading>
-        {
-          skills.map(skill => (
-            <Grid templateColumns="repeat(7, 1fr)" p={1} key={skill.id} alignItems="center">
-              <GridItem colSpan="1"><Image src={skill.image} boxSize={["30px", "50px", "70px"]} alt={skill.name} /></GridItem>
-              <GridItem colSpan="3"><Text as="em" fontWeight="bold" fontSize={['sm', 'md', 'xl']}>{skill.name} - ({skill.level}%)</Text></GridItem>
-              <GridItem colSpan="3"><Progress value={skill.level} size="xs" colorScheme="blue" isAnimated /></GridItem>
-            </Grid>
-          ))
-        }
-      </Flex>
-    </Flex>
+    <Fragment>
+      <ButtonBack />
+      <SkillsList />
+    </Fragment>
   )
 }
 
