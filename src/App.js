9e9eb588fee2dcde2cfd103c959a23f8b94c4afe
leoD-E1 <leodangeli11@gmail.com>
2022-05-24 03:02:03 -0300

    [update] added a new project in works.json

--- src/App.js
+++ src/App.js
@@ -49,7 +49,6 @@ const App = () => {
     fetchUser();
     fetchWorks();
     fetchSkills();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
--- src/components/SkillsList.js
+++ src/components/SkillsList.js
@@ -1,20 +1,27 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import { Heading, Flex } from '@chakra-ui/react'
-import LevelSkills from './reciclable/LevelSkills'
+import React from "react";
+import { useSelector } from "react-redux";
+import { Heading, Flex } from "@chakra-ui/react";
+import LevelSkills from "./reciclable/LevelSkills";
 
 const SkillsList = () => {
-
-  const skills = useSelector(state => state.skill)
+  const skills = useSelector((state) => state.skill);
 
   return (
-    <Flex pt={5} px={3} justifyContent="center" h="93vh" alignItems="center"/* bgColor="#1B1F24" */>
+    <Flex
+      pt={5}
+      px={3}
+      justifyContent="center"
+      h="93vh"
+      alignItems="center" /* bgColor="#1B1F24" */
+    >
       <Flex direction="column" w={["90%", "80%", "70%", "60%"]}>
-        <Heading mb={5} align="center">Some Skills</Heading>
+        <Heading mb={5} align="center">
+          Some Skills
+        </Heading>
         <LevelSkills skills={skills} />
       </Flex>
     </Flex>
-  )
-}
+  );
+};
 
-export default SkillsList
+export default SkillsList;
